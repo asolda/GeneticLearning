73be72266000e519db10c717d3ce9d25c0a0fa4b
asolda <a.solda@studenti.unisa.it>
2016-12-14 10:07:11 +0100

    Refactoring: moving to Keras

---
+++ algorithms/tf.gen.py
@@ -0,0 +1,76 @@
+import random
+import sys
+from deap import creator, base, tools, algorithms
+import tensorflow as tf
+import numpy as np
+from keras.models import load_model
+
+#problem parameters
+membersize = 100 #length for each indivudual
+poplen = 1000 #population dimension
+min_val = 0 #minimum value used when generating random individuals
+max_val = 5 #maximum value used when generating random individuals
+
+NGEN = 20 #number of generations for each run
+
+verbose = True
+
+if len(sys.argv) != 2:
+    print("Usage:", str(sys.argv[0]), "model.h5")
+    exit()
+
+modelfile = str(sys.argv[1])
+
+model = load_model(modelfile)
+
+
+if(verbose):
+    print("Classifier loaded")
+
+#single objective function algorithm
+creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+creator.create("Individual", list, fitness=creator.FitnessMax)
+
+toolbox = base.Toolbox()
+
+toolbox.register("attr_bool", random.randint, min_val, max_val)
+toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, n=membersize)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+#fitness function based on classifier
+def nneval(individual):
+    y = np.array(individual).reshape(1, -1)
+    cl = model.predict(y)
+    # one-hot encoded prediction
+    if cl[0][0] == 1:
+        return 1 - model.predict_proba(y, verbose = 0)[0][0],
+    else:
+        return model.predict_proba(y, verbose = 0)[0][1],
+
+toolbox.register("evaluate", nneval)
+toolbox.register("mate", tools.cxTwoPoint)
+toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
+toolbox.register("select", tools.selTournament, tournsize=3)
+
+population = toolbox.population(n=poplen)
+
+#performing genetic algorithm with classifier as fitness function
+for gen in range(NGEN):
+        if(verbose):
+            print("Gen:", gen, "out of", NGEN)
+        offspring = algorithms.varAnd(population, toolbox, cxpb=0.5, mutpb=0.1)
+        fits = toolbox.map(toolbox.evaluate, offspring)
+        for fit, ind in zip(fits, offspring):
+            ind.fitness.values = fit
+        population = toolbox.select(offspring, k=len(population))
+
+outfile = "ft.population." + str(NGEN) + ".csv"
+
+if(verbose):
+    print("Saving population to", outfile)
+
+with open(outfile, 'w') as f:
+    for elem in population:
+        for value in elem[:len(sample) - 2]:
+            f.write(str(value) + ', ')
+        f.write(str(elem[len(sample) - 1]) + "\n")
--- classifiers/tf.clf.py
+++ classifiers/tf.clf.py
@@ -1,24 +1,30 @@
 from __future__ import division
 import sys
-import tensorflow as tf
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.optimizers import SGD, Adam, RMSprop
 import numpy as np
 import time
 import pandas as pd
 
-if len(sys.argv) != 3:
-    print("Usage:", str(sys.argv[0]), "dataset.csv testset.csv")
+if len(sys.argv) != 4:
+    print("Usage:", str(sys.argv[0]), "dataset.csv testset.csv prefix_to_model")
     exit()
 
 # Datasets
 TRAINING = str(sys.argv[1])
 TEST = str(sys.argv[2])
+MODEL = str(sys.argv[3])
 
 # Load datasets
-training_set = pd.read_csv(TRAINING, dtype=np.int, header = 0)
-test_set = pd.read_csv(TEST, dtype=np.int, header = 0)
+training_set = pd.read_csv(TRAINING, dtype=np.float, header = 0)
+test_set = pd.read_csv(TEST, dtype=np.float, header = 0)
 
 num_features = training_set.shape[1] - 1
 num_labels = 2 #0 or 1
+
+batch_size = 128
+nb_epoch = 20
 
 training_set = training_set.as_matrix()
 test_set = test_set.as_matrix()
@@ -42,120 +48,36 @@ def toOneHot(list):
 y_train = toOneHot(y_train)
 y_test = toOneHot(y_test)
 
-# TRAINING SESSION PARAMETERS
-# number of times we iterate through training data
-numEpochs = 27000
-# a smarter learning rate for gradientOptimizer
-learningRate = tf.train.exponential_decay(
-    learning_rate=0.0008,
-    global_step= 1,
-    decay_steps=training_set.shape[0],
-    decay_rate= 0.95,
-    staircase=True
-)
+x_train = np.array(x_train)
+x_test = np.array(x_test)
 
-#placeholders
-X = tf.placeholder(tf.float32 , [None, num_features])
-yGold = tf.placeholder(tf.float32, [None, num_labels])
+# Model creation
+model = Sequential()
+model.add(Dense(512, input_shape=(num_features,)))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(Dense(num_labels))
+model.add(Activation('softmax'))
 
-# Values are randomly sampled from a Gaussian with a standard deviation of:
-#     sqrt(6 / (numInputNodes + numOutputNodes + 1))
+model.summary()
 
-weights = tf.Variable(tf.random_normal(
-    [num_features, num_labels],
-    mean=0,
-    stddev=(np.sqrt(6/num_features + num_labels+1)),
-    name="weights"
-))
+model.compile(loss='categorical_crossentropy',
+              optimizer=RMSprop(),
+              metrics=['accuracy'])
 
-bias = tf.Variable(tf.random_normal(
-    [1,num_labels],
-    mean=0,
-    stddev=(np.sqrt(6/num_features+num_labels+1)),
-    name="bias"
-))                      
+history = model.fit(x_train, y_train,
+                    batch_size=batch_size, nb_epoch=nb_epoch,
+                    verbose=1, validation_data=(x_test, y_test))
 
-# INITIALIZE our weights and biases
-init_OP = tf.initialize_all_variables()
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
 
-# PREDICTION ALGORITHM i.e. FEEDFORWARD ALGORITHM
-apply_weights_OP = tf.matmul(X, weights, name="apply_weights")
-add_bias_OP = tf.add(apply_weights_OP, bias, name="add_bias") 
-activation_OP = tf.nn.sigmoid(add_bias_OP, name="activation")
+model_file_name = MODEL + TRAINING.rsplit('/', 1)[-1] + '.h5'
 
-# COST FUNCTION i.e. MEAN SQUARED ERROR
-cost_OP = tf.nn.l2_loss(activation_OP-yGold, name="squared_error_cost")
+print("Saving model to", model_file_name)
 
-
-# OPTIMIZATION ALGORITHM i.e. GRADIENT DESCENT
-training_OP = tf.train.GradientDescentOptimizer(learningRate).minimize(cost_OP)
-
-# Create a tensorflow session
-sess = tf.Session()
-
-# Initialize all tensorflow variables
-sess.run(init_OP)
-
-# argmax(activation_OP, 1) gives the label our model thought was most likely
-# argmax(yGold, 1) is the correct label
-correct_predictions_OP = tf.equal(tf.argmax(activation_OP,1),tf.argmax(yGold,1))
-# False is 0 and True is 1, what was our average?
-accuracy_OP = tf.reduce_mean(tf.cast(correct_predictions_OP, "float"))
-# Summary op for regression output
-activation_summary_OP = tf.histogram_summary("output", activation_OP)
-# Summary op for accuracy
-accuracy_summary_OP = tf.scalar_summary("accuracy", accuracy_OP)
-# Summary op for cost
-cost_summary_OP = tf.scalar_summary("cost", cost_OP)
-# Summary ops to check how variables (W, b) are updating after each iteration
-weightSummary = tf.histogram_summary("weights", weights.eval(session=sess))
-biasSummary = tf.histogram_summary("biases", bias.eval(session=sess))
-# Merge all summaries
-all_summary_OPS = tf.merge_all_summaries()
-# Summary writer
-writer = tf.train.SummaryWriter("summary_logs", sess.graph)
-
-# Initialize reporting variables
-cost = 0
-diff = 1
-
-# Training epochs
-for i in range(numEpochs):
-    if i > 1 and diff < .0001:
-        print("change in cost %g; convergence."%diff)
-        break
-    else:
-        # Run training step
-        step = sess.run(training_OP, feed_dict={X: x_train, yGold: y_train})
-        # Report occasional stats
-        if i % 10 == 0:
-            # Generate accuracy stats on test data
-            summary_results, train_accuracy, newCost = sess.run(
-                [all_summary_OPS, accuracy_OP, cost_OP], 
-                feed_dict={X: x_train, yGold: y_train}
-            )
-            # Write summary stats to writer
-            writer.add_summary(summary_results, i)
-            # Re-assign values for variables
-            diff = abs(newCost - cost)
-            cost = newCost
-
-            #generate print statements
-            print("step %d, training accuracy %g"%(i, train_accuracy))
-            print("step %d, cost %g"%(i, newCost))
-            print("step %d, change in cost %g"%(i, diff))
-
-# Using test data to find out accuracy
-print("final accuracy on test set: %s" %str(sess.run(
-    accuracy_OP, 
-    feed_dict={X: x_test, yGold: y_test})))
-
-modelfile = TRAINING + ".tfmodel.ckpt"
-print("Saving model to", modelfile)
-# Create Saver
-saver = tf.train.Saver()
-# Save variables to .ckpt file
-saver.save(sess, modelfile)
-
-# Close tensorflow session
-sess.close()
+model.save(model_file_name)
